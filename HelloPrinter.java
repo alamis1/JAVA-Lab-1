3b2aa7e3fd84930e2b0ace1737bbaa07b96b52b6
alamis1 <46724550+alamis1@users.noreply.github.com>
2019-01-15 14:09:44 -0500

    Add files via upload

---
+++ HelloPrinter.java
@@ -0,0 +1,21 @@
+/********************
+HelloPrinter.java
+*Imran Alam
+*lab 1
+*1/15
+*****/
+public class Hello {
+	public static void printHeading() {
+		/*five print statements of strings followed by a blank line*/
+		System.out.println("Imran Alam");
+		System.out.println("HelloPrinter");
+		System.out.println("Lab 1");
+		System.out.println("1/15/19");
+		System.out.println("255 002");
+		System.out.println();
+	}
+	public static void main(String [] args){
+		printHeading();
+		System.out.println("Hello");
+	}
+}
